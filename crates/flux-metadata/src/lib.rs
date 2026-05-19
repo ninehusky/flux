8966ed64a0c7fa8184a17cc62e50457ddae68a60
Nico Lehmann <nico.lehmannm@gmail.com>
2026-05-19 13:51:53 -0400

    Move specs to Rc

--- crates/flux-metadata/src/lib.rs
+++ crates/flux-metadata/src/lib.rs
@@ -38,7 +38,6 @@ use rustc_data_structures::{
     fx::FxHashMap,
     unord::{ExtendUnord, UnordMap},
 };
-use rustc_hash::FxHashMap as RustcFxHashMap;
 use rustc_hir::{
     def::{CtorKind, DefKind},
     def_id::{LOCAL_CRATE, LocalDefId},
@@ -180,7 +179,7 @@ pub struct Tables<K: Eq + Hash> {
     func_span: UnordMap<FluxId<K>, Span>,
     sort_decl_param_count: UnordMap<FluxId<K>, usize>,
     no_panic: UnordMap<K, bool>,
-    no_panic_specs: RustcFxHashMap<DefId, PanicSpec>,
+    no_panic_specs: UnordMap<DefId, PanicSpec>,
 }
 
 impl CStore {
@@ -324,14 +323,16 @@ impl CrateStore for CStore {
         self.local_tables[&krate].normalized_defns.clone()
     }
 
-    fn inferred_no_panic(&self, krate: CrateNum) -> rustc_hash::FxHashMap<DefId, PanicSpec> {
+    fn inferred_no_panic(&self, krate: CrateNum) -> Rc<UnordMap<DefId, PanicSpec>> {
         // TODO: Some transitive deps (e.g. `hashbrown`) have no flux metadata. Return
         // an empty map (conservative: MightPanic) until the proper fix is in place.
         // See notes/hashbrown-inferred-no-panic.txt.
-        self.local_tables
-            .get(&krate)
-            .map(|t| t.no_panic_specs.clone())
-            .unwrap_or_default()
+        Rc::new(
+            self.local_tables
+                .get(&krate)
+                .map(|t| t.no_panic_specs.clone())
+                .unwrap_or_default(),
+        )
     }
 
     fn has_crate(&self, krate: CrateNum) -> bool {
@@ -362,6 +363,7 @@ impl CrateMetadata {
             FluxDefId::to_index,
         );
         encode_flux_defs(genv, &mut local_tables);
+        local_tables.no_panic_specs = (*genv.inferred_no_panic_crate(LOCAL_CRATE)).clone();
 
         let mut extern_tables = Tables::default();
         encode_def_ids(
@@ -378,7 +380,6 @@ impl CrateMetadata {
 
 fn encode_flux_defs(genv: GlobalEnv, tables: &mut Tables<DefIndex>) {
     tables.normalized_defns = genv.normalized_defns(LOCAL_CRATE);
-    tables.no_panic_specs = genv.inferred_no_panic_crate(LOCAL_CRATE);
 
     for (def_id, item) in genv.fhir_iter_flux_items() {
         match item {
--- crates/flux-middle/src/cstore.rs
+++ crates/flux-middle/src/cstore.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use rustc_hash::FxHashMap;
+use rustc_data_structures::unord::UnordMap;
 use rustc_hir::def_id::CrateNum;
 use rustc_span::def_id::DefId;
 
@@ -38,7 +38,7 @@ pub trait CrateStore {
     fn func_span(&self, def_id: FluxDefId) -> Option<rustc_span::Span>;
     fn sort_decl_param_count(&self, def_id: FluxDefId) -> Option<usize>;
     fn no_panic(&self, def_id: DefId) -> Option<bool>;
-    fn inferred_no_panic(&self, krate: CrateNum) -> FxHashMap<DefId, PanicSpec>;
+    fn inferred_no_panic(&self, krate: CrateNum) -> Rc<UnordMap<DefId, PanicSpec>>;
     fn has_crate(&self, krate: CrateNum) -> bool;
 }
 
